6ea01b357b665e86d4ab3d08755c83729ab031dd
KHALFALLAH Razzak <khalfallah.razzak@gmail.com>
2022-04-16 03:56:57 +0200

    ajout de l'envoi de mail

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,14 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(MailerInterface $mailer): Response
     {
         $prenom = 'Razzak';
+
+
         return $this->render('accueil/index.html.twig');
     }
 }
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,16 +7,43 @@ use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            $name = $data->getName();
+            $firstname = $data->getFirstname();
+            $mail = $data->getMail();
+            $nameos = $name . ' ' . $firstname . ' ' . $mail;
+
+            $object = $data->getObject();
+            $subject = $data->getSubject();
+
+            $email = (new Email())
+                ->from(new Address($mail, $nameos))
+                ->to("khalfallah.razzak@gmail.com")
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                ->replyTo($mail)
+                ->subject($subject)
+                ->text($object)
+                ;
+
+            $mailer->send($email);
+        }
+
 
         return $this->render('contact/index.html.twig', [
             'formulaire' => $form->createView()
