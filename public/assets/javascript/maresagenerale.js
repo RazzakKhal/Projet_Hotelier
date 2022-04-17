296e1e618e3059944e915cd0c93a16df9c297688
KHALFALLAH Razzak <khalfallah.razzak@gmail.com>
2022-04-17 22:31:20 +0200

    r

--- public/assets/javascript/maresagenerale.js
+++ public/assets/javascript/maresagenerale.js
@@ -60,10 +60,10 @@ function deblocageetab(){
 
         // requetes mes dates en ajax
         let url = '/reserv/resa' + '/' + valeuretab + '/' + valeurroom;
-        let dates;
+        let dates = null;
         axios.get(url).then(function (response) {
 
-            dates = response.data;
+            dates = response.data();
             // je récupère ma valeur date début et date fin entré ( parse les met en timestamp milliseconde et je les converti en seconde pour comparer par la suite)
             let datedebutmili = Date.parse(valeurstartm.innerHTML + ' '+ valeurstartj.innerHTML + ','+ valeurstarty.innerHTML);
             let datedebut = datedebutmili / 1000;
