9ffe6afed8dfd1e3931c396b2d05f83f4083b3e3
KHALFALLAH Razzak <khalfallah.razzak@gmail.com>
2022-04-18 02:39:23 +0200

    refonte migration au propre

--- migrations/Version20220417235447.php
+++ migrations/Version20220417235447.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220412234434 extends AbstractMigration
+final class Version20220417235447 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,7 @@ final class Version20220412234434 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, mail VARCHAR(255) NOT NULL, object LONGTEXT NOT NULL, subject VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE etablissement (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, adress VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE etablissement_room (etablissement_id INT NOT NULL, room_id INT NOT NULL, INDEX IDX_B516678CFF631228 (etablissement_id), INDEX IDX_B516678C54177093 (room_id), PRIMARY KEY(etablissement_id, room_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, etablissement_id INT DEFAULT NULL, room_id INT DEFAULT NULL, client_id INT DEFAULT NULL, start DATE DEFAULT NULL, end DATE DEFAULT NULL, INDEX IDX_42C84955FF631228 (etablissement_id), INDEX IDX_42C8495554177093 (room_id), INDEX IDX_42C8495519EB6921 (client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -36,18 +37,6 @@ final class Version20220412234434 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE etablissement_room DROP FOREIGN KEY FK_B516678CFF631228');
-        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955FF631228');
-        $this->addSql('ALTER TABLE `user` DROP FOREIGN KEY FK_8D93D649FF631228');
-        $this->addSql('ALTER TABLE etablissement_room DROP FOREIGN KEY FK_B516678C54177093');
-        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C8495554177093');
-        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C8495519EB6921');
-        $this->addSql('DROP TABLE etablissement');
-        $this->addSql('DROP TABLE etablissement_room');
-        $this->addSql('DROP TABLE reservation');
-        $this->addSql('DROP TABLE room');
-        $this->addSql('DROP TABLE `user`');
-        $this->addSql('DROP TABLE messenger_messages');
+
     }
 }
